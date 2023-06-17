d6edae49f7099a9a1812923543194e470c427110
lmfreire <l.matheus.freire10@gmil.com>
2023-06-17 12:08:42 -0300

    Primeiro desafio dio

---
+++ desafio01.rb
@@ -0,0 +1,14 @@
+# Criando_seu_Primeiro_Projeto_com_Ruby
+puts "Informe seu nome: "
+nome = gets.chomp
+
+puts "Informe seu Sobrenome: "
+sobrenome = gets.chomp
+
+puts "Informe sua Idade: "
+idade = gets.chomp
+puts `clear`
+
+puts "##################################################"
+puts "Bem vindo #{nome} #{sobrenome}, #{idade}"
+puts "##################################################"
